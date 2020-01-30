68b19859856a95f59d39287d99320fb7a070c25e
Nick Koch <marauder30@gmail.com>
2020-01-29 21:11:49 -0700

    wrote first method in orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,12 @@
 const connection = require("./connection.js");
 
 var orm = {
-    selectAll: function() {
-
+    selectAll: function(tableInput) {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, tableInput, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
     },
     insertOne: function() {
 
