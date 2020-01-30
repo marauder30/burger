22532a043a5f49753b0fd1fb0595f7dcd16ac1d3
Nick Koch <marauder30@gmail.com>
2020-01-29 20:56:25 -0700

    step 2 config setup complete

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const express = require("express");
+// var orm = require("./config/orm.js");
+
+//  Find all the parties ordering by the lowest cost to the highest cost.
+// orm.selectAndOrder("party_name", "parties", "party_cost");
+
+// orm.selectAndOrder("client_name", "clients", "id");
+
+//  Find a party in the parties table by a party name of Cigar.
+// orm.selectWhere("parties", "party_name", "Cigar");
+
+// orm.selectWhere("clients", "client_name", "Brianne");
+
+//  Find the client who is attending the most parties.
+// orm.findWhoHasMost("client_name", "id", "clients", "parties");
+
+// orm.findWhoHasMost("party_name", "id", "parties", "clients");
