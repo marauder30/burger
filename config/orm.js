1fc558bb796399465c71e81a389646881bf710ee
Nick Koch <marauder30@gmail.com>
2020-01-29 21:00:24 -0700

    step 3 config setup complete

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+const connection = require("./connection.js");
+
+var orm = {
+    selectAll: function() {
+
+    },
+    insertOne: function() {
+
+    },
+    updateOne: function() {
+
+    }
+};
+
+module.exports = orm;
