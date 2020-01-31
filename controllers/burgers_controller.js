e762e5e6b710d552fdf93fbd5e13d0644c930f8a
Nick Koch <marauder30@gmail.com>
2020-01-30 20:35:30 -0700

    filled out controller and orm, added burger-block and partial dir

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,5 +3,49 @@ const router = express.Router();
 
 const burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create(["name", "eaten"], [req.body.name, req.body.eaten], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      eaten: req.body.eaten
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+    let condition = `id = ${req.params.id}`;
+  
+    burger.delete(condition, function(result) {
+      if (!result.affectedRows) {
+        return res.status(404).end();
+      } 
+      res.status(200).end(); 
+    });
+});
 
 module.exports = router;
